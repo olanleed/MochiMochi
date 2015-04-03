94359322931f34681f77cc01b1e7687416dd5332
olanleed <olanleed@gmail.com>
2015-04-03 22:29:55 +0900

    変数名を変更

--- src/online/adagrad_rda.hpp
+++ src/online/adagrad_rda.hpp
@@ -7,21 +7,21 @@
 
 class ADAGRAD_RDA {
 private :
-  const int DIM;
-  const double LAMBDA;
+  const int kDim;
+  const double kLambda;
 
 private :
-  const double ETA = 1.0;
+  const double kEta = 1.0;
 
 private :
   Eigen::VectorXd _w;
   Eigen::VectorXd _g;
   Eigen::VectorXd _h;
-  int _t = 0;
+  int _timestep = 0;
 
 public :
-  ADAGRAD_RDA(const int dim, const double lambda) : DIM(dim), LAMBDA(lambda) {
-    _w = _g = _h = Eigen::VectorXd::Zero(DIM);
+  ADAGRAD_RDA(const int dim, const double lambda) : kDim(dim), kLambda(lambda) {
+    _w = _g = _h = Eigen::VectorXd::Zero(kDim);
   }
 
   virtual ~ADAGRAD_RDA() { }
@@ -36,21 +36,21 @@ public :
 
   void update(const Eigen::VectorXd& feature, const int label) {
     if (suffer_loss(feature, label) <= 0.0) { return ; }
-    _t++;
-    for (std::size_t i = 0; i < DIM; i++) {
+    _timestep++;
+    for (std::size_t i = 0; i < kDim; i++) {
       const double g = -label * feature[i];
 
       _g[i] += g;
       _h[i] += g * g;
 
       const int sign = _g[i] >= 0 ? 1 : -1;
-      const double eta = ETA / std::sqrt(_h[i]);
-      const double u = std::abs(_g[i]) / _t;
+      const double eta = kEta / std::sqrt(_h[i]);
+      const double u = std::abs(_g[i]) / _timestep;
 
-      if (u <= LAMBDA) {
+      if (u <= kLambda) {
         _w[i] = 0.0;
       } else {
-        _w[i] = -sign * eta * _t * (u - LAMBDA);
+        _w[i] = -sign * eta * _timestep * (u - kLambda);
       }
     }
 
