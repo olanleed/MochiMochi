e2ff2b557449765bdc1b1b77e3ea50e3420fa96f
olanleed <olanleed@gmail.com>
2015-04-04 13:06:00 +0900

    定数名をgoogleコーディング規約に従わせた

--- src/online/scw.hpp
+++ src/online/scw.hpp
@@ -7,9 +7,9 @@
 
 class SCW {
 private :
-  const int DIM;
-  const double C;
-  const double PHI;
+  const int kDim;
+  const double kC;
+  const double kPhi;
 
 private :
   Eigen::MatrixXd _sigma;
@@ -22,37 +22,37 @@ private :
 
 public :
   SCW(const int dim, const double c, const double eta)
-    : DIM(dim), C(c), PHI(cdf(eta)) {
+    : kDim(dim), kC(c), kPhi(cdf(eta)) {
     static_assert(std::numeric_limits<decltype(dim)>::max() > 0, "Dimension Error. (Dimension > 0)");
     static_assert(std::numeric_limits<decltype(c)>::max() > 0, "Hyper Parameter Error. (c > 0)");
     static_assert(std::numeric_limits<decltype(eta)>::max() > 0, "Hyper Parameter Error. (η > 0)");
 
-    _sigma = Eigen::MatrixXd::Identity(DIM, DIM);
-    _mu = Eigen::VectorXd::Zero(DIM);
+    _sigma = Eigen::MatrixXd::Identity(kDim, kDim);
+    _mu = Eigen::VectorXd::Zero(kDim);
   }
 
   virtual ~SCW() { }
 
   double suffer_loss(const Eigen::VectorXd& x, const int label) {
-    return std::max(0.0, PHI * std::sqrt(x.transpose() * _sigma * x) - label * _mu.dot(x));
+    return std::max(0.0, kPhi * std::sqrt(x.transpose() * _sigma * x) - label * _mu.dot(x));
   }
 
   double calculate_alpha(const double m, const double n, const double v, const double ganma) {
-    const auto numerator = -(2.0 * m * n + PHI * PHI * m * v) + ganma;
-    const auto denominator = 2.0 * (n * n + n * v * PHI * PHI);
+    const auto numerator = -(2.0 * m * n + kPhi * kPhi * m * v) + ganma;
+    const auto denominator = 2.0 * (n * n + n * v * kPhi * kPhi);
     return std::max(0.0, numerator / denominator);
   }
 
   double calculate_beta(const double alpha, const double v) {
-    const auto u = std::pow(-alpha * v * PHI + 4.0 * v , 2.0) / 4.0;
-    return alpha * PHI / (std::sqrt(u) + v * alpha * PHI);
+    const auto u = std::pow(-alpha * v * kPhi + 4.0 * v , 2.0) / 4.0;
+    return alpha * kPhi / (std::sqrt(u) + v * alpha * kPhi);
   }
 
   void update(const Eigen::VectorXd& x, const int label) {
     const double v = x.transpose() * _sigma * x;
     const auto m = label * (_mu.dot(x));
-    const auto n = v + 1.0 / 2.0 * C;
-    const auto ganma = PHI * std::sqrt(PHI * PHI * m * m * v * v + 4.0 * n * v * (n + v * PHI * PHI));
+    const auto n = v + 1.0 / 2.0 * kC;
+    const auto ganma = kPhi * std::sqrt(kPhi * kPhi * m * m * v * v + 4.0 * n * v * (n + v * kPhi * kPhi));
     const auto alpha = calculate_alpha(m, n, v, ganma);
     const auto beta = calculate_beta(alpha, ganma);
 
