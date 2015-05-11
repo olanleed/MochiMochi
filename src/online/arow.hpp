b6fd21fec65f9a3e05597ce46b5f5d9ce0fde9ce
olanleed <olanleed@gmail.com>
2015-05-11 19:23:10 +0900

    AROWにシリアライズ/デシリアライズ機能を追加

--- src/online/arow.hpp
+++ src/online/arow.hpp
@@ -3,6 +3,13 @@
 
 #include <Eigen/Dense>
 #include <cstdbool>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/split_member.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <fstream>
 #include "utility.hpp"
 
 class AROW {
@@ -69,6 +76,46 @@ public :
     return calculate_margin(x) > 0.0 ? 1 : -1;
   }
 
+  void save(const std::string& filename) {
+    std::ofstream ofs(filename);
+    assert(ofs);
+    boost::archive::text_oarchive oa(ofs);
+    oa << *this;
+    ofs.close();
+  }
+
+  void load(const std::string& filename) {
+    std::ifstream ifs(filename);
+    assert(ifs);
+    boost::archive::text_iarchive ia(ifs);
+    ia >> *this;
+    ifs.close();
+  }
+
+private :
+  friend class boost::serialization::access;
+  BOOST_SERIALIZATION_SPLIT_MEMBER();
+  template <class Archive>
+  void save(Archive& ar, const unsigned int version) const {
+    std::vector<double> covariances_vector(_covariances.data(), _covariances.data() + _covariances.size());
+    std::vector<double> means_vector(_means.data(), _means.data() + _means.size());
+    ar & boost::serialization::make_nvp("covariances", covariances_vector);
+    ar & boost::serialization::make_nvp("means", means_vector);
+    ar & boost::serialization::make_nvp("Dimension", const_cast<std::size_t&>(kDim));
+    ar & boost::serialization::make_nvp("R", const_cast<double&>(kR));
+  }
+
+  template <class Archive>
+  void load(Archive& ar, const unsigned int version) {
+    std::vector<double> covariances_vector;
+    std::vector<double> means_vector;
+    ar & boost::serialization::make_nvp("covariances", covariances_vector);
+    ar & boost::serialization::make_nvp("means", means_vector);
+    ar & boost::serialization::make_nvp("Dimension", const_cast<std::size_t&>(kDim));
+    ar & boost::serialization::make_nvp("R", const_cast<double&>(kR));
+    _covariances = Eigen::Map<Eigen::VectorXd>(&covariances_vector[0], covariances_vector.size());
+    _means = Eigen::Map<Eigen::VectorXd>(&means_vector[0], means_vector.size());
+  }
 };
 
 #endif //SRC_ONLINE_AROW_HPP_
