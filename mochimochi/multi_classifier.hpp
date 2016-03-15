666db0ca2689f4613481c1cf0debea4aac371641
olanleed <olanleed@gmail.com>
2016-03-15 20:49:09 +0900

    multi_classifier.hppを追加

---
+++ mochimochi/multi_classifier.hpp
@@ -0,0 +1,9 @@
+#ifndef MOCHIMOCHI_MULTI_CLASSIFIER_HPP_
+#define MOCHIMOCHI_MULTI_CLASSIFIER_HPP_
+
+#include "./classifier/multi/marow.hpp"
+#include "./classifier/multi/mscw.hpp"
+#include "./classifier/multi/mnherd.hpp"
+#include "./classifier/multi/mpa.hpp"
+
+#endif //MOCHIMOCHI_MULTI_CLASSIFIER_HPP_
